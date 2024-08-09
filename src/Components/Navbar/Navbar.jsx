616829251ad956f1fde0986a69fd454ca0609078
Dagim <dagimgetaW27@gmail.com>
2024-08-09 22:47:01 +0300

    add link to redirect to home page when click logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import upload_icon from "../../assets/upload.png";
 import more_icon from "../../assets/more.png";
 import notification_icon from "../../assets/notification.png";
 import profile_icon from "../../assets/jack.png";
+import { Link } from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
 const Navbar = ({ setSidebar }) => {
@@ -18,7 +19,9 @@ const Navbar = ({ setSidebar }) => {
           onClick={() => setSidebar((prev) => (prev === false ? true : false))}
           alt="menu icon"
         />
-        <img className="logo" src={logo} alt="logo" />
+        <Link to="/">
+          <img className="logo" src={logo} alt="logo" />
+        </Link>
       </div>
       <div className="nav-middle flex-div">
         <div className="search-box flex-div">
