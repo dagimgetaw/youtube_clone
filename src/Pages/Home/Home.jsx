9caff70f9c73f2b47b84df799f24e41e86a5c038
Dagim <dagimgetaW27@gmail.com>
2024-08-09 22:46:12 +0300

    home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,21 @@
 import "./home.css";
 import Sidebar from "../../Components/Sidebar/Sidebar.jsx";
 import Feed from "../../Components/Feed/Feed.jsx";
+import { useState } from "react";
 
 // eslint-disable-next-line react/prop-types
 const Home = ({ sidebar }) => {
+  const [category, setCategory] = useState(0);
+
   return (
     <>
-      <Sidebar sidebar={sidebar} />
+      <Sidebar
+        sidebar={sidebar}
+        category={category}
+        setCategory={setCategory}
+      />
       <div className={`container ${sidebar ? "" : "large-container"}`}>
-        <Feed />
+        <Feed category={category} />
       </div>
     </>
   );
