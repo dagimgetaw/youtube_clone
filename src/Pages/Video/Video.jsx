d948a932b013ee8a7a0822790fdb93c9abebe3cd
Dagim <dagimgetaW27@gmail.com>
2024-08-10 03:29:18 +0300

    update

--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -2,7 +2,6 @@ import { useParams } from "react-router-dom";
 import PlayVideo from "../../Components/PlayVideo/PlayVideo";
 import Recommended from "../../Components/Recommended/Recommended";
 import "./video.css";
-import Get from "../../Components/Feed/Get";
 
 // eslint-disable-next-line react/prop-types
 const Video = () => {
